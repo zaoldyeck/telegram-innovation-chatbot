25ff72d35eb9993b3bcfc4b2ebc8fe1d446be122
zaoldyeck <zaoldyeck9970@gmail.com>
2018-06-29 16:41:28 +0800

    Update welcome message

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ app = Flask(__name__)
 # Initial bot by Telegram access token
 bot = telegram.Bot(token=(config['TELEGRAM']['ACCESS_TOKEN']))
 
-welcome_message = '親愛的主人，您可以問我\n' \
-                  '天氣，例如：「高雄天氣如何」\n' \
+welcome_message = 'Hello~ 本 Bot 是用 http://bit.ly/2KxJF1F 開源程式碼所完成\n\n' \
+                  '您可以問我\n' \
+                  '天氣，例如：「台北天氣如何」\n' \
                   '百科，例如：「川普是誰」\n' \
                   '新聞，例如：「今日新聞」\n' \
                   '音樂，例如：「我想聽周杰倫的等你下課」\n' \
@@ -35,17 +36,10 @@ welcome_message = '親愛的主人，您可以問我\n' \
                   '股票，例如：「台積電的股價」\n' \
                   '食譜，例如：「蛋炒飯怎麼做」\n' \
                   '聊天，例如：「你好嗎」'
-reply_keyboard_markup = ReplyKeyboardMarkup([['高雄天氣如何'],
-                                             ['川普是誰'],
-                                             ['今日新聞'],
-                                             ['我想聽周杰倫的等你下課'],
-                                             ['現在時間'],
-                                             ['我想聽水調歌頭這首詩'],
-                                             ['講個笑話'],
-                                             ['說個故事'],
-                                             ['台積電的股價'],
-                                             ['蛋炒飯怎麼做'],
-                                             ['你好嗎']])
+reply_keyboard_markup = ReplyKeyboardMarkup([['播放陳奕迅的歌'],
+                                             ['播放Cmon in~專輯的歌'],
+                                             ['播放誰來剪月光'],
+                                             ['播放金曲獎類型的歌']])
 
 
 @app.route('/hook', methods=['POST'])
@@ -77,7 +71,7 @@ def reply_handler(bot, update):
 def error_handler(bot, update, error):
     """Log Errors caused by Updates."""
     logger.error('Update "%s" caused error "%s"', update, error)
-    update.message.reply_text('對不起主人，我需要多一點時間來處理 Q_Q')
+    update.message.reply_text('對不起唷~ 我需要多一點時間來處理 Q_Q')
 
 
 # New a dispatcher for bot
